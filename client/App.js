5c6b7dda8a7ec551673070eefa78d2a913687217
Benitaaaa <benital.2021@scis.smu.edu.sg>
2024-01-10 13:08:40 +0800

    add native-notify

--- client/App.js
+++ client/App.js
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import registerNNPushToken from 'native-notify';
 
 export default function App() {
+  registerNNPushToken(18122, 'dVHwMdzg6cW9cdVqiIDmM7');
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
