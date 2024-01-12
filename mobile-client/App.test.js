d3559b13d34c9b1a9144902147e666a3bd965faa
Benitaaaa <benital.2021@scis.smu.edu.sg>
2024-01-12 19:16:40 +0800

    Update App.test.js

--- mobile-client/App.test.js
+++ mobile-client/App.test.js
@@ -3,7 +3,9 @@ import App from './App';
 
 describe("App", () => {
     it("has 1 child", () => {
+        jest.useFakeTimers();
         const tree = renderer.create(<App />).toJSON();
         expect(tree.children.length).toBe(1);
+        jest.runAllTimers();
     });
 });
