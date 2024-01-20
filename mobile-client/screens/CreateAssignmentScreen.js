afeb4cbee091e390ba61b532ca1f803a64495a5b
tiarahimawan <tiaramariah.2021@scis.smu.edu.sg>
2024-01-20 11:11:32 +0800

    Remove photo

--- mobile-client/screens/CreateAssignmentScreen.js
+++ mobile-client/screens/CreateAssignmentScreen.js
@@ -5,10 +5,12 @@ import AnimatedPlaceholderInput from '../components/ui/animateTextInput';
 import * as ImagePicker from "expo-image-picker";
 
 function CreateAssignmentScreen({  }){
-    console.log('create assignment screen')
+    const [assignmentName, setAssignmentName] = useState();
+    const [assignmentDesc, setAssignmentDesc] = useState();
+    const [assignmentDeadline, setAssignmentDeadline] = useState();
+
     const [image, setImage] = useState();
     const uploadImage = async(mode) => {
-        console.log('upload button pressed');
         try {
             let result = {};
             if (mode==='gallery'){
@@ -16,8 +18,8 @@ function CreateAssignmentScreen({  }){
                 await ImagePicker.requestMediaLibraryPermissionsAsync()
                 result = await ImagePicker.launchImageLibraryAsync({
                     mediaTypes: ImagePicker.MediaTypeOptions.Images,
-                    allowsEditing: true,
-                    aspect: [1,1],
+                    // allowsEditing: true,
+                    aspect: [4,3],
                     quality: 1,
                 })
             } 
@@ -42,9 +44,19 @@ function CreateAssignmentScreen({  }){
     
         }
     };
+    
+    const removeImage = async() => {
+        console.log("Removing image");
+        try {
+            saveImage(null);
+        } catch ({message}){
+            alert(message);
+        }
+    };
 
     const saveImage = async(image) => {
         try {
+            console.log(image)
             setImage(image);
             // setModalVisible(false);
         } catch (error){
@@ -56,13 +68,17 @@ function CreateAssignmentScreen({  }){
             <AnimatedPlaceholderInput 
                 placeholder="Assignment Name" 
                 secureTextEntry={false} 
-                textInputConfig={{autoCapitalize: 'words'}}>
+                textInputConfig={{autoCapitalize: 'words'}}
+                value={assignmentName}
+                onChangeText={setAssignmentName}>    
             </AnimatedPlaceholderInput>
 
             <AnimatedPlaceholderInput 
                 placeholder="Description" 
                 secureTextEntry={false} 
-                textInputConfig={{multiline: true}}>
+                textInputConfig={{multiline: true}}
+                value={assignmentDesc}
+                onChangeText={setAssignmentDesc}>
             </AnimatedPlaceholderInput>
 
             <AnimatedPlaceholderInput 
@@ -70,34 +86,33 @@ function CreateAssignmentScreen({  }){
                 secureTextEntry={false} 
                 textInputConfig={{ 
                     maxLength: 10
-                }}>
+                }}
+                value={assignmentDeadline}
+                onChangeText={setAssignmentDeadline}>
             </AnimatedPlaceholderInput>
 
-            <Image
+            <TouchableOpacity style={theme.button} onPress={() => uploadImage("gallery")}>
+                <Text style={theme.buttonText }>Upload Photo</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={theme.button} onPress={() => {console.log('Remove button pressed');
+            removeImage()}}>
+                <Text style={theme.buttonText }>Remove Photo</Text>
+            </TouchableOpacity>
+            
+            {image && (
+                <Image
+                    source={{ uri: image }}
+                    style={styles.image}
+                />
+            )}
+
+            {/* <Image
                 source={{ uri: image }}
                 // source={{ uri }}
                 style={[
                     styles.image,
                 ]}
-            />
-            <TouchableOpacity style={theme.button} onPress={() => uploadImage("gallery")}>
-                <Text style={theme.buttonText }>Upload</Text>
-            </TouchableOpacity>
-            
-            {/* <Input label="Assignment Name" textInputConfig={{
-                // keyboardType: 'decimal-pad',
-                autoCapitalize: 'words',
-                onChangeText:assignmentNameChangedHandler,
-            }} />
-            <Input label="Description" textInputConfig={{
-                multiline: true,
-                // autocorrect: false // default is true
-            }}/>
-            <Input label="Deadline" textInputConfig={{
-                placeholder: 'DD-MM-YYYY',
-                maxLength: 10,
-                onChangeText: () => {}
-            }}/> */}
+            /> */}
         </View>
         
     )
