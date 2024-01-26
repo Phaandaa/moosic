e4cb0919ef4011a973cea19ec631a4ddf5aad808
Davina Wijaya <phandavina@gmail.com>
2024-01-27 02:01:40 +0800

    fix: include password in dto

--- server/src/main/java/com/example/server/dto/CreateUserDTO.java
+++ server/src/main/java/com/example/server/dto/CreateUserDTO.java
@@ -11,13 +11,14 @@ public class CreateUserDTO {
     private String name;
     private String email;
     private String role;
+    private String password;
     private HashMap<String, String> info;
 
     public CreateUserDTO() {
 
     }
 
-    public CreateUserDTO(String name, String email, String role, HashMap<String, String> info) {
+    public CreateUserDTO(String name, String email, String role, String password, HashMap<String, String> info) {
         this.name = name;
         this.email = email;
         this.role = role;
