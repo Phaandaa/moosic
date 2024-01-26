f1f7a4221d8994b04ffbb70eebc80989a623106d
Davina Wijaya <phandavina@gmail.com>
2024-01-27 02:03:32 +0800

    feature: add seperate create teacher, studnt , admin function

--- server/src/main/java/com/example/server/service/UserService.java
+++ server/src/main/java/com/example/server/service/UserService.java
@@ -34,39 +34,60 @@ public class UserService {
 
     @Transactional
     public User createUser(CreateUserDTO userDTO) {
-        if (userDTO != null) {
-            // TODO: insert firebase request here 
-            // send request to firebase, await response for user id 
-            String id = "3f0f96a1-4949-4750-89ef-29961e8e621b"; // shud be response from firebase
-            String name = userDTO.getName();
-            String email = userDTO.getEmail();
-            String role = userDTO.getRole();
-            User newUser = new User(id, name, email, role);
-            userRepository.save(newUser);
-            switch (role) {
-                case "Student":
-                    createStudent(id, userDTO);
-                    break;
-                case "Teacher":
-                    createTeacher(id);
-                    break;
-                default:
-                    break;
-            }
-            return newUser;
+        if (userDTO == null) {
+            throw new IllegalArgumentException();
         }
-        return null;
+        String email = userDTO.getEmail();
+        String password = userDTO.getPassword();
+        // TODO: insert firebase request here
+        // send request to firebase, await response for user id
+        String id = "randomid3"; // shud be response from firebase
+        String name = userDTO.getName();
+        String role = userDTO.getRole();
+        User newUser = new User(id, name, email, role);
+        userRepository.save(newUser);
+        switch (role) {
+            case "Student":
+                createStudent(id, name, userDTO);
+                break;
+            case "Teacher":
+                createTeacher(id, name);
+                break;
+            default:
+                break;
+        }
+        return newUser;
     }
 
-    private void createStudent(String id, CreateUserDTO userDTO) {
+    public User createAdmin(CreateUserDTO userDTO) {
+        if (userDTO == null) {
+            throw new IllegalArgumentException();
+        }
+        String email = userDTO.getEmail();
+        String password = userDTO.getPassword();
+        String name = userDTO.getName();
+        // TODO: send request to firebase
+        String id = "randomid3";
+        User newUser = new User(id, name, email, "Admin");
+        userRepository.save(newUser);
+        return newUser;
+    }
+
+    private void createStudent(String id, String name, CreateUserDTO userDTO) {
         String teacherId = userDTO.getInfo().get("teacher_id");
         String instrument = userDTO.getInfo().get("instrument");
-        Student newStudent = new Student(id, 0, teacherId, new ArrayList<>(), instrument);
+        Student newStudent = new Student(id, 0, teacherId, name, new ArrayList<>(), instrument);
+        Optional<Teacher> selectedTeacher = teacherRepository.findById(teacherId);
+        if (selectedTeacher.isPresent()) {
+            Teacher teacher = selectedTeacher.get();
+            teacher.addStudent(id);
+            teacherRepository.save(teacher);
+        }
         studentRepository.save(newStudent);
     }
 
-    private void createTeacher(String id) {
-        Teacher newTeacher = new Teacher(id, new ArrayList<>());
+    private void createTeacher(String id, String name) {
+        Teacher newTeacher = new Teacher(id, name, new ArrayList<>());
         teacherRepository.save(newTeacher);
     }
 
