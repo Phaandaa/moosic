1390478dc179edf459baf469808956eb96b94a9e
Benitaaaa <benital.2021@scis.smu.edu.sg>
2024-01-27 02:23:54 +0800

    fix: .env file
    making the priority .env file first instead of application.properties

--- server/src/test/java/com/example/server/ServerApplicationTests.java
+++ server/src/test/java/com/example/server/ServerApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.example.server.ServerApplication;
 
 @SpringBootTest
 class ServerApplicationTests {
