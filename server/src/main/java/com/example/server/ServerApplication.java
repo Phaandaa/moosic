075339bba319a32428b2fb243a796f6c1342b811
Davina Wijaya <phandavina@gmail.com>
2024-01-27 15:18:37 +0800

    feature: merge authentication and user creation

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -16,17 +16,21 @@ public class ServerApplication {
 		String mongodbDB = dotenv.get("MONGODB_DB");
 		String firebaseApiKey = dotenv.get("FIREBASE_API_KEY");
 
-		SpringApplication app = new SpringApplication(ServerApplication.class);
-		app.setDefaultProperties(
-			Map.of("spring.data.mongodb.uri", mongodbURI,
-				"spring.data.mongodb.database", mongodbDB,
-				"firebase.apiKey", firebaseApiKey)
-		);
+		System.setProperty("spring.data.mongodb.uri", mongodbURI);
+		System.setProperty("spring.data.mongodb.database", mongodbDB);
+		System.setProperty("firebase.apiKey", firebaseApiKey);
 
-		if (mongodbDB == null || mongodbURI == null || firebaseApiKey == null) {
-			System.out.println("Please set MONGODB_URI, MONGODB_DB, and FIREBASE_API_KEY in .env file");
-			System.exit(1);
-		}
+		SpringApplication.run(ServerApplication.class, args);
+		// app.setDefaultProperties(
+		// 	Map.of("spring.data.mongodb.uri", mongodbURI,
+		// 		"spring.data.mongodb.database", mongodbDB,
+		// 		"firebase.apiKey", firebaseApiKey)
+		// );
+
+		// if (mongodbDB == null || mongodbURI == null || firebaseApiKey == null) {
+		// 	System.out.println("Please set MONGODB_URI, MONGODB_DB, and FIREBASE_API_KEY in .env file");
+		// 	System.exit(1);
+		// }
 		// System.out.println("Loaded environment variables:");
         // System.out.println("MONGODB_URI: " + mongodbURI);
         // System.out.println("MONGODB_DB: " + mongodbDB);
@@ -35,7 +39,7 @@ public class ServerApplication {
 		// System.setProperty("spring.data.mongodb.uri", mongodbURI);
 		// System.setProperty("spring.data.mongodb.database", mongodbDB);
 		// System.setProperty("firebase.apiKey", firebaseApiKey);
-		app.run(args);
+		// app.run(args);
 	}
 
 }
