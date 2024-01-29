aeb705e3f81841f87ef2c4fa22d1de3b4d43db42
Angie <louiseangiee@gmail.com>
2024-01-29 15:14:55 +0800

    feat: upload video

--- mobile-client/screens/PracticeScreen.js
+++ mobile-client/screens/PracticeScreen.js
@@ -3,14 +3,18 @@ import { View, StyleSheet, Image, TouchableOpacity, Text, ScrollView, Alert, But
 import theme from './styles/theme';
 import AnimatedPlaceholderInput from '../components/ui/animateTextInput';
 import * as ImagePicker from "expo-image-picker";
-import * as DocumentPicker from "expo-document-picker";
-import { Audio } from 'expo-av';
+import { Audio, Video, ResizeMode} from 'expo-av';
+import LottieView from 'lottie-react-native';
 
 function PracticeScreen({navigation}){
     const [title, setTitle] = useState('');
     const [comment, setComment] = useState('');
     const [recording, setRecording] = useState();
     const [permissionResponse, requestPermission] = Audio.usePermissions();
+    const cancelIcon = require('../assets/cancel.png');
+    const [videos, setVideos] = useState([]);
+    const [loadingStates, setLoadingStates] = useState({});
+
 
     const submitHandler = () => {
         console.log('Practice Title on Submit:', title, 'Type:', typeof title );
@@ -20,6 +24,7 @@ function PracticeScreen({navigation}){
         const practiceData = {
             title: title,
             comment: comment,
+            videos: videos,
         };
         console.log(practiceData)
 
@@ -64,7 +69,41 @@ function PracticeScreen({navigation}){
         );
         const uri = recording.getURI();
         console.log('Recording stopped and stored at', uri);
-      }
+    }
+
+    const uploadVideo = async(mode) => {
+        try {
+            let result = {};
+            if (mode==='gallery'){
+                console.log('gallery')
+                await ImagePicker.requestMediaLibraryPermissionsAsync()
+                result = await ImagePicker.launchImageLibraryAsync({
+                    mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+                    // allowsEditing: true,
+                    aspect: [4,3],
+                    quality: 1,
+                })
+            } 
+            if(!result.canceled){
+                console.log('result not cancelled')
+                await saveVideo(result.assets[0].uri);
+            }
+        } catch (error){
+            alert("error uploading video:"+ error.message
+            );
+            setModalVisible(false);
+
+        }
+    };
+    
+    const removeVideo = (index) => {
+        setVideos((currentVideos) => currentVideos.filter((_, i) => i !== index));
+    };
+
+    const saveVideo = (newVideo) => {
+        setVideos((currentVideos) => [...currentVideos, newVideo]);
+        setLoadingStates((currentLoadingStates) => ({ ...currentLoadingStates, [newVideo]: true }));
+    };
     
     
     return (
@@ -91,13 +130,46 @@ function PracticeScreen({navigation}){
                 <TouchableOpacity style={theme.button2} onPress={recording ? stopRecording : startRecording}>
                     <Text style={theme.buttonText}>{recording ? 'Stop Recording' : 'Start Recording'}</Text>
                 </TouchableOpacity>
+                <TouchableOpacity style={theme.button2} onPress={() => uploadVideo("gallery")}>
+                    <Text style={theme.buttonText}>Upload Video</Text>
+                </TouchableOpacity>
             </View>
+
+            {/* Render each image with a remove button next to it */}
+            <ScrollView horizontal>
+                {videos.map((uri, index) => (
+                    <View key={uri} style={styles.imageContainer}>
+                        {loadingStates[uri] && (
+                            <LottieView
+                                source={require('../assets/loading_fyp.json')} // Update with your Lottie file path
+                                autoPlay
+                                loop
+                                style={styles.lottie}
+                            />
+                        )}
+                        <Video
+                            source={{ uri }}
+                            style={styles.video}
+                            resizeMode={ResizeMode.COVER}
+                            useNativeControls
+                            onLoadStart={() => setLoadingStates({ ...loadingStates, [uri]: true })}
+                            onLoad={() => setLoadingStates({ ...loadingStates, [uri]: false })}
+                        />
+                        <TouchableOpacity onPress={() => removeVideo(index)} style={styles.removeButton}>
+                            {/* <Text style={theme.buttonText}>x</Text> */}
+                            <Image source={cancelIcon} style={styles.cancelIcon} /> 
+                        </TouchableOpacity>
+                    </View>
+                ))}
+            </ScrollView>
 
             {/* Submit button */}
             <TouchableOpacity style={theme.button} onPress={submitHandler}>
                 <Text style={theme.buttonText}>Submit</Text>
             </TouchableOpacity>
         </View>
+
+        
     </ScrollView> 
     )
 };
@@ -160,5 +232,16 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between', // This will space out the buttons evenly
         marginTop: 20,
         paddingHorizontal: 10, // Add some padding on the sides
-    }
+    },
+    video: {
+        width: 150,
+        height: 150,
+        // other styles you might want to add
+    },
+    lottie: {
+        position: 'absolute',
+        width: '100%',
+        size: '50%',
+        height: '100%',
+    },
 });
