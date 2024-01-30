e8acf6aae2e7a23a5df13440fafe66abe2296b8f
Audrey Kurniawan <audrey.kurniawan03@gmail.com>
2024-01-30 23:18:13 +0800

    cache skeleton finished

---
+++ mobile-client/cacheAction.js
@@ -0,0 +1,12 @@
+// cacheActions.js
+export const SET_CACHE = 'SET_CACHE';
+export const CLEAR_CACHE = 'CLEAR_CACHE';
+
+export const setCache = (key, value) => ({
+  type: SET_CACHE,
+  payload: { key, value },
+});
+
+export const clearCache = () => ({
+  type: CLEAR_CACHE,
+});
