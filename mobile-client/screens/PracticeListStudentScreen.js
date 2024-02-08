9d7ddc42568f93784781b245dcd4c183cc7ce2a1
tiarahimawan <tiaramariah.2021@scis.smu.edu.sg>
2024-02-08 23:24:38 +0700

    feat: play video recording (student)

--- mobile-client/screens/PracticeListStudentScreen.js
+++ mobile-client/screens/PracticeListStudentScreen.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import { View, StyleSheet, Image, TouchableOpacity, Text, ScrollView, Alert} from 'react-native';
+import React, {useState} from 'react';
+import { View, StyleSheet, Image, TouchableOpacity, Text, ScrollView, Alert, Dimensions, Button} from 'react-native';
+import Modal from 'react-native-modal';
 import theme from './styles/theme';
 import ProvidePracticeFeedbackScreen from './ProvidePracticeFeedbackScreen';
+import { Audio, Video, ResizeMode} from 'expo-av';
 import { useSelector } from 'react-redux';
+
 function PracticeListStudentScreen({navigation}){
     const practiceData = useSelector(state => state.cache.practiceData);
+    const [isModalVisible, setModalVisible] = useState(false);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+
+    const toggleModal = () => {
+        setModalVisible(!isModalVisible);
+    };
+
+    const openVideo = (uri) => {
+        setSelectedVideo(uri);
+        toggleModal(); 
+    }
+
+
     return (
         <View style={theme.container}> 
-        {/* Student 1  */}
             <TouchableOpacity style={theme.card}>
                 <View style={theme.cardTextContainer}>
                     <Text style={theme.cardTextBold}>{practiceData.title}</Text>
@@ -15,13 +30,35 @@ function PracticeListStudentScreen({navigation}){
                 </View>
                 <View style={theme.buttonContainer}>
                     <TouchableOpacity style={theme.smallButton}>
-                        <Text style={theme.smallButtonText}>Recording</Text>
+                        <Text style={theme.smallButtonText} onPress={() => openVideo(practiceData.videos[0])}>Recording</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={theme.smallButton}>
-                        <Text style={theme.smallButtonText} onPress={() => navigation.navigate(ProvidePracticeFeedbackScreen)}>Feedback</Text>
+                        <Text style={theme.smallButtonText} onPress={() => navigation.navigate('ProvidePracticeFeedbackScreen')}>Feedback</Text>
                     </TouchableOpacity>
                 </View>
             </TouchableOpacity>
+
+            {/* Modal for viewing the video */}
+            <Modal
+                    isVisible={isModalVisible}
+                    animationType="slide"
+                    transparent={true}
+                    visible={isModalVisible}
+                >
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            {selectedVideo && (
+                                <Video
+                                source={{ uri: selectedVideo }}
+                                style={styles.modalVideo}
+                                resizeMode={ResizeMode.CONTAIN}
+                                shouldPlay
+                            />
+                            )}
+                            <Button title="Close" onPress={toggleModal} />
+                        </View>
+                    </View>
+                </Modal>
         </View>
     )
 }
@@ -61,5 +98,30 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 14,
         textAlign: 'center',
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 25,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    modalVideo: {
+        width: Dimensions.get('window').width * 0.8, // 80% of window width
+        height: Dimensions.get('window').height * 0.7
     }
 })
