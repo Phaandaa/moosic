55d15944451ad2407bea0a17e2ce9bdd2fc49a13
tiarahimawan <tiaramariah.2021@scis.smu.edu.sg>
2024-02-08 23:36:13 +0700

    feat: add My Practice Log access from homepage

--- mobile-client/screens/home.js
+++ mobile-client/screens/home.js
@@ -44,6 +44,7 @@ const HomeScreen = ({ navigation }) => {
     { id: 'knowledge', color: '#686BFF', title: 'Knowledge', subtitle: 'Hand and Finger Work', iconName: 'bulb', navigationPage: 'CreateAssignmentScreen', iconColor: 'white', buttonText: 'Continue' },
     { id: 'practice', color: '#EE97BC', title: 'Practice', subtitle: 'Record My Practice Sessions', iconName: 'musical-notes', navigationPage: 'PracticeScreen', iconColor: 'white', buttonText: 'View' },
     { id: 'assignments', color: '#466CFF', title: 'Assignments', subtitle: 'View My Assignments', iconName: 'people', navigationPage: 'ViewAssignmentsScreen', iconColor: 'white', buttonText: 'View' },
+    { id: 'practicelog', color: '#EE97BC', title: 'Practice Log', subtitle: 'View My Practice Sessions', iconName: 'musical-notes', navigationPage: 'PracticeListStudentScreen', iconColor: 'white', buttonText: 'View' },
     // Add more modules specific to student
   ];
 
