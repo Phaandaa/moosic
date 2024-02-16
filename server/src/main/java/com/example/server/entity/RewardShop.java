aa6dce1cfb5277194a5a0c79877cf7342ac154aa
Davina Wijaya <phandavina@gmail.com>
2024-02-16 20:55:33 +0800

    feature: reward shop service

--- server/src/main/java/com/example/server/entity/RewardShop.java
+++ server/src/main/java/com/example/server/entity/RewardShop.java
@@ -21,14 +21,15 @@ public class RewardShop {
     @Field(name = "description")
     private String description;
 
-    @Field(name = "description")
+    @Field(name = "points")
     private Integer points;
 
-    @Field(name = "availability")
-    private Integer availability;
+    @Field(name = "stock")
+    private Integer stock;
 
-    @Field(name = "admin_password")
-    private String adminPassword;
+    // just use their login password because if each item has their own password, not v effective
+    // @Field(name = "admin_password")
+    // private String adminPassword; 
 
     @Field(name = "limitation")
     private Integer limitation;
@@ -36,21 +37,23 @@ public class RewardShop {
     @Field(name = "image_link")
     private String imageLink;
 
-    public RewardShop(String description, int points, int availability, String adminPassword, int limitation,
-            String imageLink) {
+    @Field(name = "type")
+    private String type;
+
+    public RewardShop(String description, Integer points, Integer stock, Integer limitation, String imageLink,
+            String type) {
         this.description = description;
         this.points = points;
-        this.availability = availability;
-        this.adminPassword = adminPassword;
+        this.stock = stock;
         this.limitation = limitation;
         this.imageLink = imageLink;
+        this.type = type;
     }
 
     @Override
     public String toString() {
-        return "RewardShop [rewardId=" + id + ", description=" + description + ", points=" + points
-                + ", availability=" + availability + ", adminPassword=" + adminPassword + ", limitation=" + limitation
-                + ", imageLink=" + imageLink + "]";
+        return "RewardShop [id=" + id + ", description=" + description + ", points=" + points + ", stock=" + stock
+                + ", limitation=" + limitation + ", imageLink=" + imageLink + ", type=" + type + "]";
     }
 
 }
