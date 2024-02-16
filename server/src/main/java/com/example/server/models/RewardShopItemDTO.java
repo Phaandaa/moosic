db8f19402fbc70fd9f04f37c43b19c5ba60f225c
Davina Wijaya <phandavina@gmail.com>
2024-02-16 20:55:33 +0800

    feature: reward shop service

---
+++ server/src/main/java/com/example/server/models/RewardShopItemDTO.java
@@ -0,0 +1,18 @@
+package com.example.server.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RewardShopItemDTO {
+    private String description;
+    private Integer points;
+    private Integer stock;
+    private Integer limitation;
+    private String type;
+}
