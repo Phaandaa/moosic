b2a0e5ad6c8637b168dfb02349dd8ff494b88ff7
Davina Wijaya <phandavina@gmail.com>
2024-02-16 18:48:10 +0800

    perf: rewards shop entity improvement

--- server/src/main/java/com/example/server/entity/RewardShop.java
+++ server/src/main/java/com/example/server/entity/RewardShop.java
@@ -2,21 +2,38 @@ package com.example.server.entity;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "rewards_shop")
 public class RewardShop {
     @Id
-    private String rewardId;
+    private String id;
+
+    @Field(name = "description")
     private String description;
-    private int points;
-    private int availability;
+
+    @Field(name = "description")
+    private Integer points;
+
+    @Field(name = "availability")
+    private Integer availability;
+
+    @Field(name = "admin_password")
     private String adminPassword;
-    private int limitation;
+
+    @Field(name = "limitation")
+    private Integer limitation;
+
+    @Field(name = "image_link")
     private String imageLink;
 
     public RewardShop(String description, int points, int availability, String adminPassword, int limitation,
@@ -27,13 +44,11 @@ public class RewardShop {
         this.adminPassword = adminPassword;
         this.limitation = limitation;
         this.imageLink = imageLink;
-
-    
     }
 
     @Override
     public String toString() {
-        return "RewardShop [rewardId=" + rewardId + ", description=" + description + ", points=" + points
+        return "RewardShop [rewardId=" + id + ", description=" + description + ", points=" + points
                 + ", availability=" + availability + ", adminPassword=" + adminPassword + ", limitation=" + limitation
                 + ", imageLink=" + imageLink + "]";
     }
