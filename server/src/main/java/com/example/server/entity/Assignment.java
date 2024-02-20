872d75d8dc36b67977df526616a48bdb8e33eea9
Davina Wijaya <phandavina@gmail.com>
2024-02-20 16:17:50 +0800

    comment: add todo in assignment.java

--- server/src/main/java/com/example/server/entity/Assignment.java
+++ server/src/main/java/com/example/server/entity/Assignment.java
@@ -15,7 +15,8 @@ import lombok.Setter;
 @Setter
 @Document(collection = "assignments")
 public class Assignment {
-    // TODO: check implementation on how deadline can be used as Date object 
+    // TODO: check implementation on how deadline can be used as Date object
+    // TODO: add created date field 
     @Id
     private String assignmentId;
     private String title;
