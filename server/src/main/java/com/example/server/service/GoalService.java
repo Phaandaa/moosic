20de8c6d37b755607a2e6ad91f148ab026d6b413
Davina Wijaya <phandavina@gmail.com>
2024-02-22 00:37:49 +0800

    feature: get all goals by teacher id in dao

---
+++ server/src/main/java/com/example/server/service/GoalService.java
@@ -0,0 +1,30 @@
+package com.example.server.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.server.dao.GoalRepository;
+
+@Service
+public class GoalService {
+
+    @Autowired
+    private GoalRepository goalRepository;
+
+    // create goal
+
+    // add goal item to goal
+
+    // remove goal item in goal
+
+    // edit goal item in goal
+
+    // change status for goal item
+
+    // mark goal as completed
+
+    // get goal by student id 
+
+    // get goal by teacher id
+
+}
