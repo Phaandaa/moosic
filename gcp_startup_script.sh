fc5ae8e631eac637c0864d3b44fd72681d9ccdc0
Davina Wijaya <phandavina@gmail.com>
2024-03-07 01:52:53 +0800

    feature: startup script for gcp instance

--- gcp_startup_script.sh
+++ gcp_startup_script.sh
@@ -1,12 +1,27 @@
-#!/bin/bash
-sudo apt install unzip -y
-sudo apt install zip -y
+#! /bin/bash
+
+apt update
+apt install -y unzip zip git maven
+
 curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
+source "/root/.sdkman/bin/sdkman-init.sh"
 sdk install java 21.0.2-amzn
-sudo apt install -y git
-git clone https://github.com/Phaandaa/moosic.git
-cd moosic/server
-sudo apt install -y maven
-# Remember to upload env and serviceAccountKey.json, and automate this process if possible
-mvn spring-boot:run
+
+if id "moosicuser" &>/dev/null; then
+    echo 'User moosicuser already exists.'
+else
+    useradd -m -s /bin/bash moosicuser
+fi
+
+git clone https://github.com/Phaandaa/moosic.git /opt/moosic
+
+chown -R moosicuser:moosicuser /opt/moosic
+
+gsutil cp gs://cloud-setup/server/.env /opt/moosic/server/
+gsutil cp gs://cloud-setup/server/serviceAccountKey.json /opt/moosic/server/src/main/resources/
+chown moosicuser:moosicuser /opt/moosic/server/.env
+chown moosicuser:moosicuser /opt/moosic/server/src/main/resources/serviceAccountKey.json
+
+sudo -u moosicuser sh -c 'cd /opt/moosic/server && mvn install'
+
+sudo -u moosicuser sh -c 'cd /opt/moosic/server && nohup mvn spring-boot:run &'
