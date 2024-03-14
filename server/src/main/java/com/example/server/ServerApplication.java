105c0497615d8ae3fae162f300f018d436b471b3
Davina Wijaya <phandavina@gmail.com>
2024-03-14 16:55:42 +0800

    perf: improve code for GCS uploading files

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -18,6 +18,7 @@ public class ServerApplication {
 		String gcpProjectId = dotenv.get("GCP_PROJECT_ID");
 		String pubSubTopicName = dotenv.get("PUB_SUB_TOPIC_NAME");
 		String googleApplicationCredentials = dotenv.get("GOOGLE_APPLICATION_CREDENTIALS");
+		String gcsBucketName = dotenv.get("GCS_BUCKET_NAME");
 
 		System.setProperty("spring.data.mongodb.uri", mongodbURI);
 		System.setProperty("spring.data.mongodb.database", mongodbDB);
@@ -25,6 +26,7 @@ public class ServerApplication {
 		System.setProperty("spring.cloud.gcp.project-id", gcpProjectId);
 		System.setProperty("spring.cloud.gcp.pubsub.topic-name", pubSubTopicName); 
 		System.setProperty("spring.cloud.gcp.credentials.location", "file:" + googleApplicationCredentials);
+		System.setProperty("gcs-bucket", "gs://" + gcsBucketName);
 		
 		SpringApplication.run(ServerApplication.class, args);
 	}
