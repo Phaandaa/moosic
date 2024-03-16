c99a5148eab14798c53be2a1a7bcf5d5889954c9
Benitaaaa <benital.2021@scis.smu.edu.sg>
2024-03-17 00:35:12 +0800

    fix: serverApplication.java

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -20,11 +20,10 @@ public class ServerApplication {
 		String mongodbURI = dotenv.get("MONGODB_URI");
 		String mongodbDB = dotenv.get("MONGODB_DB");
 		String firebaseApiKey = dotenv.get("FIREBASE_API_KEY");
-
-		// Print out the loaded environment variables
-        System.out.println("MONGODB_URI: " + mongodbURI);
-        System.out.println("MONGODB_DB: " + mongodbDB);
-        System.out.println("FIREBASE_API_KEY: " + firebaseApiKey);
+		String gcpProjectId = dotenv.get("GCP_PROJECT_ID");
+		String pubSubTopicName = dotenv.get("PUB_SUB_TOPIC_NAME");
+		String googleApplicationCredentials = dotenv.get("GOOGLE_APPLICATION_CREDENTIALS");
+		String gcsBucketName = dotenv.get("GCS_BUCKET_NAME");
 
 		System.setProperty("spring.data.mongodb.uri", mongodbURI);
 		System.setProperty("spring.data.mongodb.database", mongodbDB);
