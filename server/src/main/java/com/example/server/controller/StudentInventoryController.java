c2662518bff805cd1ed8a05fc19dbdb5487d7229
Davina Wijaya <phandavina@gmail.com>
2024-03-23 00:07:59 +0800

    fix: cross origin in auth and student inventory controller

--- server/src/main/java/com/example/server/controller/StudentInventoryController.java
+++ server/src/main/java/com/example/server/controller/StudentInventoryController.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.example.server.service.StudentInventoryService;
 
 import io.swagger.v3.oas.annotations.Operation;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/student-inventory")
 public class StudentInventoryController {
