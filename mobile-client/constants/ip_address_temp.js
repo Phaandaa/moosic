cfaf265b4a8170ac087a9bad3c6443d458717e17
Audrey Kurniawan <audrey.kurniawan03@gmail.com>
2024-03-23 18:32:36 +0800

    Update ip_address_temp.js

--- mobile-client/constants/ip_address_temp.js
+++ mobile-client/constants/ip_address_temp.js
@@ -1,4 +1,4 @@
-// const IP_ADDRESS = 'https://learn2play-moosic.com'; // Replace with your own IP address
-const IP_ADDRESS = 'http://192.168.32.15:8080'; // Replace with your own IP address
+const IP_ADDRESS = 'https://learn2play-moosic.com'; // Replace with your own IP address
+// const IP_ADDRESS = 'http://192.168.32.15:8080'; // Replace with your own IP address
 
 export default IP_ADDRESS;
