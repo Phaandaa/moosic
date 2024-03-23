a8c0f09ad5575bc68ae752a59ec61e508d08bf9e
Angie <louiseangiee@gmail.com>
2024-03-23 21:10:16 +0800

    feat: implemented integration for overview component card "latest product"

--- web-client/src/sections/overview/overview-latest-products.js
+++ web-client/src/sections/overview/overview-latest-products.js
@@ -1,7 +1,7 @@
-import { formatDistanceToNow } from 'date-fns';
-import PropTypes from 'prop-types';
-import ArrowRightIcon from '@heroicons/react/24/solid/ArrowRightIcon';
-import EllipsisVerticalIcon from '@heroicons/react/24/solid/EllipsisVerticalIcon';
+import { formatDistanceToNow, parseISO } from "date-fns";
+import PropTypes from "prop-types";
+import ArrowRightIcon from "@heroicons/react/24/solid/ArrowRightIcon";
+import EllipsisVerticalIcon from "@heroicons/react/24/solid/EllipsisVerticalIcon";
 import {
   Box,
   Button,
@@ -14,80 +14,80 @@ import {
   ListItem,
   ListItemAvatar,
   ListItemText,
-  SvgIcon
-} from '@mui/material';
+  SvgIcon,
+} from "@mui/material";
+import Link from "next/link";
 
 export const OverviewLatestProducts = (props) => {
   const { products = [], sx } = props;
 
+  // Sort the products by creationTime in descending order
+  const sortedProducts = [...products].sort((a, b) => {
+    return parseISO(a.creationTime) - parseISO(b.creationTime);
+  });
+
+  // Slice the array to get the top 5 most recent products
+  const recentProducts = sortedProducts.slice(0, 5);
   return (
     <Card sx={sx}>
       <CardHeader title="Latest Products" />
       <List>
-        {products.map((product, index) => {
-          const hasDivider = index < products.length - 1;
-          const ago = formatDistanceToNow(product.updatedAt);
-
+        {recentProducts.map((product, index) => {
+          const hasDivider = index < recentProducts.length - 1;
+          const ago = product.creationTime
+            ? formatDistanceToNow(new Date(product.creationTime))
+            : "Unknown time";
           return (
-            <ListItem
-              divider={hasDivider}
-              key={product.id}
-            >
+            <ListItem divider={hasDivider} key={product.id}>
               <ListItemAvatar>
-                {
-                  product.image
-                    ? (
-                      <Box
-                        component="img"
-                        src={product.image}
-                        sx={{
-                          borderRadius: 1,
-                          height: 48,
-                          width: 48
-                        }}
-                      />
-                    )
-                    : (
-                      <Box
-                        sx={{
-                          borderRadius: 1,
-                          backgroundColor: 'neutral.200',
-                          height: 48,
-                          width: 48
-                        }}
-                      />
-                    )
-                }
+                {product.imageLink ? (
+                  <Box
+                    component="img"
+                    src={product.imageLink}
+                    sx={{
+                      borderRadius: 1,
+                      height: 48,
+                      width: 48,
+                    }}
+                  />
+                ) : (
+                  <Box
+                    sx={{
+                      borderRadius: 1,
+                      backgroundColor: "neutral.200",
+                      height: 48,
+                      width: 48,
+                    }}
+                  />
+                )}
               </ListItemAvatar>
               <ListItemText
-                primary={product.name}
-                primaryTypographyProps={{ variant: 'subtitle1' }}
+                primary={product.description}
+                primaryTypographyProps={{ variant: "subtitle1" }}
                 secondary={`Updated ${ago} ago`}
-                secondaryTypographyProps={{ variant: 'body2' }}
+                secondaryTypographyProps={{ variant: "body2" }}
               />
-              <IconButton edge="end">
-                <SvgIcon>
-                  <EllipsisVerticalIcon />
-                </SvgIcon>
-              </IconButton>
             </ListItem>
           );
         })}
       </List>
       <Divider />
-      <CardActions sx={{ justifyContent: 'flex-end' }}>
-        <Button
-          color="inherit"
-          endIcon={(
-            <SvgIcon fontSize="small">
-              <ArrowRightIcon />
-            </SvgIcon>
-          )}
-          size="small"
-          variant="text"
-        >
-          View all
-        </Button>
+      <CardActions sx={{ justifyContent: "flex-end" }}>
+        <Link href="/shop" passHref>
+          <Button
+            component="a" // This makes the Button component act as an anchor tag
+            color="inherit"
+            endIcon={
+              <SvgIcon fontSize="small">
+                <ArrowRightIcon />
+              </SvgIcon>
+            }
+            size="small"
+            variant="text"
+          >
+            View all
+          </Button>
+        </Link>
       </CardActions>
     </Card>
   );
@@ -95,5 +95,5 @@ export const OverviewLatestProducts = (props) => {
 
 OverviewLatestProducts.propTypes = {
   products: PropTypes.array,
-  sx: PropTypes.object
+  sx: PropTypes.object,
 };
