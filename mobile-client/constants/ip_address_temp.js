337b28dc2cc096b6a9424c75839ba82c198e338f
tiarahimawan <tiaramariah.2021@scis.smu.edu.sg>
2024-04-04 20:48:50 +0800

    change IP address to learn2play-moosic.com

--- mobile-client/constants/ip_address_temp.js
+++ mobile-client/constants/ip_address_temp.js
@@ -1,3 +1,3 @@
-const IP_ADDRESS = 'https://192.168.50.47:8080'; // Replace with your own IP address
+const IP_ADDRESS = 'https://learn2play-moosic.com'; // Replace with your own IP address
 
 export default IP_ADDRESS;
