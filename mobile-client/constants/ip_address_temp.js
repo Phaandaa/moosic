5e40c3544eef1743955b391adc4793bb8e098a14
tiarahimawan <tiaramariah.2021@scis.smu.edu.sg>
2024-05-04 21:14:11 +0700

    Cloud IP Address

--- mobile-client/constants/ip_address_temp.js
+++ mobile-client/constants/ip_address_temp.js
@@ -1,4 +1,4 @@
-// const IP_ADDRESS = 'https://learn2play-moosic.com';
-const IP_ADDRESS = 'http://192.168.1.47:8080';
+const IP_ADDRESS = 'https://learn2play-moosic.com';
+// const IP_ADDRESS = 'http://192.168.1.47:8080';
 
 export default IP_ADDRESS;
